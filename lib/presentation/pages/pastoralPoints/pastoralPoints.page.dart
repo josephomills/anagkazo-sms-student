383c1640002e8a60e749be1ef3c29e757fe7a0a0
Joseph Mills <joseph.mills.dev@gmail.com>
2022-09-12 21:17:46 +0000

    disciplinary and pastoral points pages added

---
+++ lib/presentation/pages/pastoralPoints/pastoralPoints.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PastoralPointsPage extends StatelessWidget {
+  const PastoralPointsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset("assets/illustrations/welcome.png"),
+    );
+  }
+}
