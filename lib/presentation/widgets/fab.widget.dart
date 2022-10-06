c7e9223450e26ff4ba8e57afe453587f4843ec8c
Joseph Mills <joseph.mills.dev@gmail.com>
2022-10-06 13:41:31 +0000

    Update fab.widget.dart

--- lib/presentation/widgets/fab.widget.dart
+++ lib/presentation/widgets/fab.widget.dart
@@ -1,4 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:student/application/core/router.core.gr.dart';
 
 class FABWidget extends StatelessWidget {
   const FABWidget({Key? key}) : super(key: key);
@@ -7,7 +10,17 @@ class FABWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       child: const Icon(Icons.qr_code_scanner),
-      onPressed: () {},
+      onPressed: (() async {
+        // Check camera permission before opening scan page
+        var status = await Permission.camera.status;
+        if (status.isDenied) {
+          await Permission.camera.request();
+        }
+
+        if (status.isGranted) {
+          context.router.push(const ScanRoute());
+        }
+      }),
     );
   }
 }
