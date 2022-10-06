86f33e5040e3df00d2b2bb62a87ecf6c38f2c725
Joseph Mills <joseph.mills.dev@gmail.com>
2022-10-06 13:39:49 +0000

    member pages added

---
+++ lib/presentation/pages/myFellowship/memberDetails.page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MemberDetailsPage extends StatelessWidget {
+  const MemberDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/pages/myFellowship/serviceDetails.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ServiceDetailsPage extends StatelessWidget {
+  const ServiceDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
