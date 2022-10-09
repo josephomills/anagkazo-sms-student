2f9dfa0f1ecad288051170a5bf933019652b9531
Joseph Mills <joseph.mills.dev@gmail.com>
2022-10-09 08:05:28 +0000

    separated scan in and out methods

--- lib/domain/attendance/scan/scan.facade.dart
+++ lib/domain/attendance/scan/scan.facade.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:student/domain/attendance/scan/scan.failure.dart';
+import 'package:student/infrastructure/attendance/models/event.object.dart';
 import 'package:student/infrastructure/attendance/models/scan.object.dart';
-import 'package:student/infrastructure/core/models/yearGroup.object.dart';
+import 'package:student/infrastructure/academics/models/yearGroup.object.dart';
 
 abstract class ScanFacade {
-  Future<Either<ScanFailure, ScanObject>> scan();
+  Future<Either<ScanFailure, ScanObject>> scanIn(
+      {required EventObject event, required DateTime dateTime});
+
+  Future<Either<ScanFailure, ScanObject>> scanOut(
+      {required EventObject event, required DateTime dateTime});
 
   Future<bool> checkValidScan({
     required DateTime scanDate,
