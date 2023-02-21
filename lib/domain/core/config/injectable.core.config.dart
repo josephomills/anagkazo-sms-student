ee0a529981898d0638638684419aaf05646324f0
Joseph Mills <joseph.mills.dev@gmail.com>
2023-02-21 14:04:40 +0000

    added injectable auth module to pre-resolve current user

--- lib/domain/core/config/injectable.core.config.dart
+++ lib/domain/core/config/injectable.core.config.dart
@@ -8,23 +8,24 @@
 import 'package:flutter/material.dart' as _i12;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart' as _i17;
 import 'package:student/application/attendance/attendance/attendance_bloc.dart'
-    as _i19;
-import 'package:student/application/attendance/scan/scan_bloc.dart' as _i21;
+    as _i20;
+import 'package:student/application/attendance/scan/scan_bloc.dart' as _i22;
 import 'package:student/application/auth/login/login_bloc.dart' as _i14;
 import 'package:student/application/dashbaord/dashboard_bloc.dart' as _i9;
 import 'package:student/application/home/home_bloc.dart' as _i13;
 import 'package:student/application/myFellowship/my_fellowship_bloc.dart'
-    as _i20;
+    as _i21;
 import 'package:student/domain/attendance/attendance.facade.dart' as _i3;
-import 'package:student/domain/attendance/scan/scan.facade.dart' as _i17;
+import 'package:student/domain/attendance/scan/scan.facade.dart' as _i18;
 import 'package:student/domain/auth/auth.facade.dart' as _i5;
 import 'package:student/domain/auth/auth_validator.dart' as _i7;
-import 'package:student/domain/core/config/injectable_modules.dart' as _i22;
+import 'package:student/domain/core/config/injectable_modules.dart' as _i23;
 import 'package:student/domain/dashboard/dashboard.facade.dart' as _i10;
 import 'package:student/domain/myFellowship/myFellowship.facade.dart' as _i15;
 import 'package:student/infrastructure/attendance/attendance.repo.dart' as _i4;
-import 'package:student/infrastructure/attendance/scan.repo.dart' as _i18;
+import 'package:student/infrastructure/attendance/scan.repo.dart' as _i19;
 import 'package:student/infrastructure/auth/auth.repo.dart' as _i6;
 import 'package:student/infrastructure/dashboard/dashboard.repo.dart' as _i11;
 import 'package:student/infrastructure/myFellowship/myFellowship.repo.dart'
@@ -35,16 +36,17 @@ import 'package:student/infrastructure/myFellowship/service/b4a.service.dart'
 // ignore_for_file: lines_longer_than_80_chars
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
+  Future<_i1.GetIt> init({
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
-  }) {
+  }) async {
     final gh = _i2.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
     final formKeyModule = _$FormKeyModule();
+    final authModule = _$AuthModule();
     gh.factory<_i3.AttendanceFacade>(() => _i4.AttendanceRepo());
     gh.factory<_i5.AuthFacade>(() => _i6.AuthRepo());
     gh.factory<_i7.AuthValidator>(() => _i7.AuthValidator());
@@ -56,14 +58,20 @@ extension GetItInjectableX on _i1.GetIt {
     gh.lazySingleton<_i14.LoginBloc>(
         () => _i14.LoginBloc(gh<_i5.AuthFacade>()));
     gh.factory<_i15.MyFellowshipFacade>(() => _i16.MyFellowshipRepo());
-    gh.factory<_i17.ScanFacade>(() => _i18.ScanRepo());
-    gh.lazySingleton<_i19.AttendanceBloc>(
-        () => _i19.AttendanceBloc(gh<_i3.AttendanceFacade>()));
-    gh.lazySingleton<_i20.MyFellowshipBloc>(
-        () => _i20.MyFellowshipBloc(gh<_i15.MyFellowshipFacade>()));
-    gh.lazySingleton<_i21.ScanBloc>(() => _i21.ScanBloc(gh<_i17.ScanFacade>()));
+    await gh.singletonAsync<_i17.ParseUser>(
+      () => authModule.currentUser,
+      preResolve: true,
+    );
+    gh.factory<_i18.ScanFacade>(() => _i19.ScanRepo());
+    gh.lazySingleton<_i20.AttendanceBloc>(
+        () => _i20.AttendanceBloc(gh<_i3.AttendanceFacade>()));
+    gh.lazySingleton<_i21.MyFellowshipBloc>(
+        () => _i21.MyFellowshipBloc(gh<_i15.MyFellowshipFacade>()));
+    gh.lazySingleton<_i22.ScanBloc>(() => _i22.ScanBloc(gh<_i18.ScanFacade>()));
     return this;
   }
 }
 
-class _$FormKeyModule extends _i22.FormKeyModule {}
+class _$FormKeyModule extends _i23.FormKeyModule {}
+
+class _$AuthModule extends _i23.AuthModule {}
--- lib/domain/core/config/injectable_modules.dart
+++ lib/domain/core/config/injectable_modules.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 @module
 abstract class FormKeyModule {
   @injectable
   GlobalKey<FormState> get formKey => GlobalKey<FormState>();
+}
+
+@module
+abstract class AuthModule {
+  @preResolve
+  @singleton
+  Future<ParseUser> get currentUser =>
+      ParseUser.currentUser() as Future<ParseUser>;
 }
