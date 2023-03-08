c68439f6f4def3d68662fe1278b90873661b716f
Joseph Mills <joseph.mills.dev@gmail.com>
2023-03-08 13:39:31 +0000

    Update home.page.dart

--- lib/presentation/pages/home/home.page.dart
+++ lib/presentation/pages/home/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student/presentation/widgets/app_bars/home.appbar.widget.dart';
 import 'package:student/presentation/widgets/cards/dashboard_card.widget.dart';
 import 'package:student/presentation/widgets/cards/quick_action.widget.dart';
 
@@ -7,71 +8,74 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: const ClampingScrollPhysics(),
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      children: [
-        // PageTitleWidget(title: "${greet()} ${user.firstname}"),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            DashboardCardWidget(
-              stat: "50",
-              label: "Academic Points",
-              color: Colors.green,
-            ),
-            DashboardCardWidget(
-              stat: "110",
-              label: "Disciplinary Points",
-              color: Color.fromARGB(255, 201, 26, 13),
-            ),
-            DashboardCardWidget(
-              stat: "12",
-              label: "Avg. Bussing",
-              color: Colors.orange,
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            DashboardCardWidget(
-              stat: "12",
-              label: "Fellowship",
-              color: Colors.blueGrey,
-            ),
-            DashboardCardWidget(
-              stat: "50",
-              label: "Income (GHC)",
-              color: Colors.deepPurple,
-            ),
-            DashboardCardWidget(
-              stat: "240",
-              label: "Anti-Brutish",
-              color: Color.fromARGB(255, 139, 154, 2),
-            ),
-          ],
-        ),
-        const SizedBox(height: 24),
-        const Text("Quick actions"),
-        const Divider(thickness: 2),
-        QuickActionWidget(
-          icon: const Icon(Icons.my_library_books),
-          title: "Open student documents",
-          onTap: () {},
-        ),
-        QuickActionWidget(
-          icon: const Icon(Icons.checklist),
-          title: "Mark fellowship meeting attendance",
-          onTap: () {},
-        ),
-        QuickActionWidget(
-          icon: const Icon(Icons.receipt),
-          title: "Fill a form (e.g. permission, registry etc)",
-          onTap: () {},
-        ),
-      ],
+    return Scaffold(
+      appBar: HomeAppBarWidget(),
+      body: ListView(
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        children: [
+          // PageTitleWidget(title: "${greet()} ${user.firstname}"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              DashboardCardWidget(
+                stat: "50",
+                label: "Academic Points",
+                color: Colors.green,
+              ),
+              DashboardCardWidget(
+                stat: "110",
+                label: "Disciplinary Points",
+                color: Color.fromARGB(255, 201, 26, 13),
+              ),
+              DashboardCardWidget(
+                stat: "12",
+                label: "Avg. Bussing",
+                color: Colors.orange,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              DashboardCardWidget(
+                stat: "12",
+                label: "Fellowship",
+                color: Colors.blueGrey,
+              ),
+              DashboardCardWidget(
+                stat: "50",
+                label: "Income (GHC)",
+                color: Colors.deepPurple,
+              ),
+              DashboardCardWidget(
+                stat: "240",
+                label: "Anti-Brutish",
+                color: Color.fromARGB(255, 139, 154, 2),
+              ),
+            ],
+          ),
+          const SizedBox(height: 24),
+          const Text("Quick actions"),
+          const Divider(thickness: 2),
+          QuickActionWidget(
+            icon: const Icon(Icons.my_library_books),
+            title: "Open student documents",
+            onTap: () {},
+          ),
+          QuickActionWidget(
+            icon: const Icon(Icons.checklist),
+            title: "Mark fellowship meeting attendance",
+            onTap: () {},
+          ),
+          QuickActionWidget(
+            icon: const Icon(Icons.receipt),
+            title: "Fill a form (e.g. permission, registry etc)",
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
