d27683cb7e1adef33210d932b5b1ad9b34a9ec03
Joseph Mills <joseph.mills.dev@gmail.com>
2023-03-08 13:49:23 +0000

    Fix nav indexes

--- lib/presentation/pages/index.page.dart
+++ lib/presentation/pages/index.page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:student/presentation/navigation/router.core.gr.dart';
 import 'package:student/presentation/widgets/bottom_nav.widget.dart';
+import 'package:student/presentation/widgets/fab.widget.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({Key? key}) : super(key: key);
@@ -12,13 +13,19 @@ class IndexPage extends StatelessWidget {
       homeIndex: NavIndex.home,
       routes: [
         const HomeRoute(),
-        const AcademicsRoute(),
         const AttendanceRoute(),
+        const AcademicsRoute(),
         MyFellowshipRoute(),
         const MoreRoute(),
       ],
       bottomNavigationBuilder: (context, tabsRouter) {
         return const BottomNavWidget();
+      },
+      floatingActionButtonBuilder: (context, tabsRouter) {
+        return (tabsRouter.activeIndex == NavIndex.home ||
+                tabsRouter.activeIndex == NavIndex.attendance)
+            ? const FABWidget()
+            : null;
       },
     );
   }
@@ -27,8 +34,8 @@ class IndexPage extends StatelessWidget {
 /// Nav index for pages
 class NavIndex {
   static const int home = 0;
-  static const int academics = 1;
-  static const int attendance = 2;
+  static const int attendance = 1;
+  static const int academics = 2;
   static const int ministry = 3;
   static const int more = 4;
 }
