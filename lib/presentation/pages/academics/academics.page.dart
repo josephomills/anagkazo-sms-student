dd99f4e925297878815ea251561117da3d4eb064
Joseph Mills <joseph.mills.dev@gmail.com>
2023-03-08 13:41:07 +0000

    Update academics.page.dart

--- lib/presentation/pages/academics/academics.page.dart
+++ lib/presentation/pages/academics/academics.page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:student/presentation/widgets/app_bars/home.appbar.widget.dart';
 
 class AcademicsPage extends StatelessWidget {
   const AcademicsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset("assets/illustrations/educator.png"),
+    return Scaffold(
+      appBar: HomeAppBarWidget(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 72),
+          child: Image.asset("assets/illustrations/education.png"),
+        ),
+      ),
     );
   }
 }
