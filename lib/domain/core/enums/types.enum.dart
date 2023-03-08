16380de4b71eced05df1a309689419bc3079a6ed
Joseph Mills <joseph.mills.dev@gmail.com>
2023-03-08 13:44:45 +0000

    Update types.enum.dart

--- lib/domain/core/enums/types.enum.dart
+++ lib/domain/core/enums/types.enum.dart
@@ -1,6 +1,20 @@
-enum EventCategory { academic, church, other }
+enum EventCategory { lecture, church, other }
+
+enum GatheringCategory { lecture, church, other }
 
 enum EventType {
+  vision,
+  pillar,
+  foundational,
+  tutorial,
+  live,
+  experience,
+  encounter,
+  assembly,
+  other,
+}
+
+enum GatheringType {
   vision,
   pillar,
   foundational,
