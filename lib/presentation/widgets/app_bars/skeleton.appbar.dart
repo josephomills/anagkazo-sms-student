10332367fa46a283e2f3561c9170c4aecf1ce8ab
Joseph Mills <joseph.mills.dev@gmail.com>
2023-03-08 13:37:01 +0000

    Create skeleton.appbar.dart

---
+++ lib/presentation/widgets/app_bars/skeleton.appbar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:skeletons/skeletons.dart';
+
+class SkeletonAppBarWidget extends StatelessWidget with PreferredSizeWidget {
+  const SkeletonAppBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: BoxDecoration(color: Theme.of(context).primaryColorLight),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 24, right: 24, top: 32),
+        child: Row(
+          children: [
+            SkeletonAvatar(
+              style: SkeletonAvatarStyle(
+                width: 60,
+                height: 60,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SkeletonLine(
+                  style: SkeletonLineStyle(
+                    height: 16,
+                    width: 100,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SkeletonLine(
+                  style: SkeletonLineStyle(
+                    height: 30,
+                    width: 180,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            const SkeletonAvatar(
+              style: SkeletonAvatarStyle(
+                width: 40,
+                height: 40,
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+}
