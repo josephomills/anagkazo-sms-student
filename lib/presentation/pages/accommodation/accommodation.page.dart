0db0c07bf65ed1d40954379f2755667d1950cfd1
Joseph Mills <joseph.mills.dev@gmail.com>
2023-03-09 13:34:11 +0000

    Create accommodation.page.dart

---
+++ lib/presentation/pages/accommodation/accommodation.page.dart
@@ -0,0 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:student/presentation/widgets/lists/empty_state.widget.dart';
+
+class AccommodationPage extends StatelessWidget {
+  const AccommodationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.router.current.meta["title"]),
+      ),
+      body: const EmptyStateWidget(
+        asset: "assets/illustrations/houses.png",
+        text: "Coming soon...",
+        spacing: 0,
+      ),
+    );
+  }
+}
